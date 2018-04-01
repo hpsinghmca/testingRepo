064fea8b31a9f0bf2f63212da9f120b18189925a
Harpreet <hpsinghmca@gmail.com>
2018-03-31 23:08:34 -0400

    Add new project

---
+++ GitTesting/GitTesting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitTesting
+//
+//  Created by Harpreet on 31/03/18.
+//  Copyright © 2018 Harpreet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
